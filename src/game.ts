197d989ccd26bddb6525ba91570494f64368ab94
Nico E <earnshaw.nico@gmail.com>
2019-01-14 11:56:33 -0300

    walk in place

--- src/game.ts
+++ src/game.ts
@@ -1,130 +1,28 @@
-// Path to follow while patrolling
-const point1 = new Vector3(5, 0, 5)
-const point2 = new Vector3(5, 0, 15)
-const point3 = new Vector3(15, 0, 15)
-const point4 = new Vector3(15, 0, 5)
 
-const myPath: Vector3[] = [point1, point2, point3, point4]
+// Create temple
+const temple = new Entity()
+temple.add(new GLTFShape('models/Temple.gltf'))
+temple.add(new Transform({
+  position: new Vector3(10, 0, 10)
+}))
 
-@Component('lerpData')
-export class LerpData {
-  origin: Vector3 = myPath[0]
-  target: Vector3 = myPath[1]
-  fraction: number = 0
-  nextPathIndex: number = 1
-  turnTime: number = 0.8
-}
+// Add temple to engine
+engine.addEntity(temple)
 
-// Walk following the points in the path
-
-export class GnarkWalk {
-  update(dt: number) {
-    let transform = gnark.get(Transform)
-    let path = gnark.get(LerpData)
-
-    if (walkClip.playing) {
-      if (path.fraction < 1) {
-        path.fraction += dt / 6
-        transform.position = Vector3.Lerp(
-          path.origin,
-          path.target,
-          path.fraction
-        )
-      } else {
-        walkClip.pause()
-        turnRClip.play()
-        path.origin = path.target
-        path.nextPathIndex += 1
-        if (path.nextPathIndex >= myPath.length) {
-          path.nextPathIndex = 0
-        }
-        path.target = myPath[path.nextPathIndex]
-        transform.lookAt(path.target)
-      }
-    }
-    else if (turnRClip.playing) {
-      if (path.turnTime > 0) {
-        path.turnTime -= dt
-      } else {
-        path.turnTime = 0.8
-        walkClip.play()
-        path.fraction = 0
-      }
-    }
-  }
-}
-
-engine.addSystem(new GnarkWalk())
-
-// React and stop walking when the user gets close enough
-
-export class BattleCry {
-  update() {
-    let transform = gnark.get(Transform)
-    let path = gnark.get(LerpData)
-    let dist = distance(transform.position, camera.position)
-    if ( dist < 16) {
-      raiseDeadClip.play()
-      walkClip.pause()
-      turnRClip.pause()
-      transform.lookAt(camera.position)
-    }
-    else if (raiseDeadClip.playing){
-      raiseDeadClip.pause()
-      walkClip.play()
-      transform.lookAt(path.target)
-    }
-  }
-}
-
-engine.addSystem(new BattleCry())
-
-// Object that tracks user position and rotation
-const camera = Camera.instance
-
-// Add Gnark
+// Create Gnark
 let gnark = new Entity()
 gnark.add(new Transform({
-  position: new Vector3(5, 0, 5),
-  scale: new Vector3(0.75, 0.75, 0.75),
-  rotation: Quaternion.Euler(0, 0, 0)
+ position: new Vector3(5, 0, 5),
+ scale: new Vector3(0.75, 0.75, 0.75)
 }))
 gnark.add(new GLTFShape('models/gnark.gltf'))
-
-// Add animations
-const walkClip = new AnimationClip('walk')
-const turnRClip = new AnimationClip('turnRight', { loop: false })
-const raiseDeadClip = new AnimationClip('raiseDead')
-gnark.get(GLTFShape).addClip(walkClip)
-gnark.get(GLTFShape).addClip(turnRClip)
-gnark.get(GLTFShape).addClip(raiseDeadClip)
-
-// Activate walk animation
-walkClip.play()
-
-// add a path data component
-gnark.add(new LerpData())
 
 // Add Gnark to engine
 engine.addEntity(gnark)
 
-// Add 3D model for scenery
-const castle = new Entity()
-castle.add(new GLTFShape('models/Pirate_Ground.gltf'))
-castle.add(new Transform({
-  position: new Vector3(10, 0, 10)
-}))
-engine.addEntity(castle)
+// Add walk animation
+const walkClip = new AnimationClip('walk')
+gnark.get(GLTFShape).addClip(walkClip)
 
-// Get distance
-/* 
-Note:
-This function really returns distance squared, as it's a lot more efficient to calculate.
-The square root operation is expensive and isn't really necessary if we compare the result to squared values.
-We also use {x,z} not {x,y}. The y-coordinate is how high up it is.
-*/
-function distance(pos1: Vector3, pos2: Vector3): number {
-  const a = pos1.x - pos2.x
-  const b = pos1.z - pos2.z
-  return a * a + b * b
-}
+// Activate walk animation
+walkClip.play()
