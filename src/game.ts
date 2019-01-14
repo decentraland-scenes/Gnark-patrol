00ee88526c38d674a509e95b37021144e63e5419
Nico E <earnshaw.nico@gmail.com>
2019-01-14 14:23:53 -0300

    lerp movement

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,13 @@
 
+// LerpData component
+@Component("lerpData")
+export class LerpData {
+  origin: Vector3 = new Vector3(5, 0, 5)
+  target: Vector3 = new Vector3(5, 0, 15)
+ fraction: number = 0
+}
+
+
 // Create temple
 const temple = new Entity()
 temple.add(new GLTFShape('models/Temple.gltf'))
@@ -17,6 +26,9 @@ gnark.add(new Transform({
 }))
 gnark.add(new GLTFShape('models/gnark.gltf'))
 
+// Add LerpData component to Gnark
+gnark.add(new LerpData())
+
 // Add Gnark to engine
 engine.addEntity(gnark)
 
@@ -31,8 +43,19 @@ walkClip.play()
 // Walk System
 export class GnarkWalk {
   update(dt: number) {
-    let increment = Vector3.Forward().scale(dt * 1.5)
-    gnark.get(Transform).translate(increment)
+    let transform = gnark.get(Transform)
+    let lerp = gnark.get(LerpData)
+    if (lerp.fraction < 1) {
+       lerp.fraction += dt / 6
+      transform.position = Vector3.Lerp(
+        lerp.origin,
+        lerp.target,
+        lerp.fraction
+      )
+      }
+     else {
+       walkClip.pause()
+    }
   }
 }
 
