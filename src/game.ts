fbbf612101c4121636ff49c6ce75c4e6a044e063
nearnshaw <nearnshaw@decentraland.org>
2019-07-16 14:45:26 -0300

    P2P

--- src/game.ts
+++ src/game.ts
@@ -8,6 +8,10 @@ const path: Vector3[] = [point1, point2, point3, point4]
 
 const TURN_TIME = 0.9
 
+// // a message bus to sync state for all players
+export const sceneMessageBus = new MessageBus()
+
+
 // LerpData component
 @Component("lerpData")
 export class LerpData {
@@ -15,6 +19,8 @@ export class LerpData {
   origin: number = 0
   target: number = 1
   fraction: number = 0
+  yelling: boolean = false
+  yellingAtPlayer: boolean = false
 }
 
 // Rotate component
@@ -28,6 +34,19 @@ export class TimeOut {
 
 // component group to hold all entities with a timeOut
 export const paused = engine.getComponentGroup(TimeOut)
+
+/// --- Define a custom type to pass in messages ---
+type yellMessage = {
+	playerPos: Vector3;
+  };
+
+type walkMessage = {
+	gnarkPos: Vector3,
+	origin: number, 
+	target: number, 
+	fraction: number
+  };
+
 
 // Create temple
 const temple = new Entity()
@@ -132,17 +151,24 @@ export class BattleCry {
     let path = gnark.getComponent(LerpData)
     let dist = distance(transform.position, camera.position)
     if ( dist < 16) {
-      if(raiseDeadClip.playing == false){
-        raiseDeadClip.reset()
-        raiseDeadClip.playing = true
-        walkClip.playing = false
-        turnRClip.playing = false
-      }
-      transform.lookAt(camera.position)
+		if (!path.yelling && !path.yellingAtPlayer){
+			//path.yelling = true
+			path.yellingAtPlayer = true
+			const action: yellMessage = {
+				playerPos: camera.position.clone()
+			}
+			sceneMessageBus.emit("yell", action)
+      	}    
     }
-    else if (raiseDeadClip.playing){
-      raiseDeadClip.stop()
-      transform.lookAt(path.array[path.target])
+    else if (path.yellingAtPlayer){
+		path.yellingAtPlayer = false
+		const action: walkMessage = {
+			gnarkPos: transform.position.clone(), 
+			origin: path.origin, 
+			target: path.target, 
+			fraction: path.fraction 
+		}
+		sceneMessageBus.emit("walk", action)
     }
   }
 }
@@ -151,6 +177,32 @@ engine.addSystem(new BattleCry())
 
 // Object that tracks user position and rotation
 const camera = Camera.instance
+
+sceneMessageBus.on("yell", (info: yellMessage) => {	
+	raiseDeadClip.reset()
+	raiseDeadClip.playing = true
+	walkClip.playing = false
+	turnRClip.playing = false
+	let transform = gnark.getComponent(Transform)
+	transform.lookAt(info.playerPos)
+	let path = gnark.getComponent(LerpData)
+	path.yelling = true
+	//log(info.playerPos)
+  });
+
+sceneMessageBus.on("walk", (info: walkMessage) => {	
+	raiseDeadClip.stop()
+	let path = gnark.getComponent(LerpData)
+	let transform = gnark.getComponent(Transform)
+	transform.position = info.gnarkPos
+	path.target = info.target
+	path.origin = info.origin
+	path.fraction = info.fraction
+	path.yelling = false
+	transform.lookAt(path.array[path.target])
+});
+
+
 
 // Get distance
 /* 
@@ -164,3 +216,21 @@ function distance(pos1: Vector3, pos2: Vector3): number {
   const b = pos1.z - pos2.z
   return a * a + b * b
 }
+
+
+// To get the initial state of the scene when joining
+sceneMessageBus.emit("getGameState",{})
+
+// To return the initial state of the scene to new players
+sceneMessageBus.on("getGameState", () => {
+	let transform = gnark.getComponent(Transform)
+	let path = gnark.getComponent(LerpData)
+	const action: walkMessage = 
+	{
+		gnarkPos: transform.position.clone(), 
+		origin: path.origin, 
+		target: path.target, 
+		fraction: path.fraction 
+	}
+	sceneMessageBus.emit("walk", action)
+});
